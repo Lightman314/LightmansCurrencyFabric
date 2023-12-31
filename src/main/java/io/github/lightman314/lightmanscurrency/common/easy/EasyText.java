7b0fefb3d29425586d0ae748d902a95463f053eb
Lightman <lightman314@gmail.com>
2023-12-31 15:21:30 -0500

    1.0.2.0 update Part 2

--- src/main/java/io/github/lightman314/lightmanscurrency/common/easy/EasyText.java
+++ src/main/java/io/github/lightman314/lightmanscurrency/common/easy/EasyText.java
@@ -1,5 +1,6 @@
 package io.github.lightman314.lightmanscurrency.common.easy;
 
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 
@@ -10,5 +11,7 @@ public final class EasyText {
     public static MutableText empty() { return Text.empty(); }
     public static MutableText literal(String literal) { return Text.literal(literal); }
     public static MutableText translatable(String translation, Object... children) { return Text.translatable(translation, children); }
+
+    public static void sendMessage(PlayerEntity player, Text message) { player.sendMessage(message); }
 
 }
